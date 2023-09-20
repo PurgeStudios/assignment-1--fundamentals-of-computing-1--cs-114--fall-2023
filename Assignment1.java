b3dc28a45333bb0942cf0e430483d5a3c47188c3
Dimitri <56205878+PurgeStudios@users.noreply.github.com>
2023-09-20 15:41:10 -0400

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,19 @@
 // Import libraries here
 
+// My attemp at my initials
+
 public class Assignment1 {
   public static void main(String[] args) {
-    // Add your code here
+    System.out.println("DDDDDDDDDDDDDDDDDD            DDDDDDDDDDDDDDDDDDDDDDDDDDD");
+    System.out.println("DDDDDDDDDDDDDDDDDDDDD       DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+    System.out.println("DDDDD            DDDDDD     DDDDDD       DDDDDDD     DDDDDD");
+    System.out.println("DDD                DDDDD    DDDDD         DDDDD       DDDDD");
+    System.out.println("DDD                DDDDD    DDDDD         DDDDD       DDDDD");
+    System.out.println("DDD                DDDDD    DDDDD         DDDDD       DDDDD");
+    System.out.println("DDD                DDDDD    DDDDD         DDDDD       DDDDD");
+    System.out.println("DDDDD            DDDDDD     DDDDD         DDDDD       DDDDD");
+    System.out.println("DDDDDDDDDDDDDDDDDDDDD       DDDDD         DDDDD       DDDDD");
+    System.out.println("DDDDDDDDDDDDDDDDDDD         DDDDD         DDDDD       DDDDD");
+
   }
 }
