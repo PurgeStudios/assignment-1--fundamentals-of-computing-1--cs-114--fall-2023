de08e3634dae807ee7a8a8b1f88bf5ec2a1c071a
Dimitri <56205878+PurgeStudios@users.noreply.github.com>
2023-09-20 17:12:47 -0400

    Update Assignment 1
    Added Question and Converter

--- Assignment1.java
+++ Assignment1.java
@@ -1,9 +1,13 @@
-// Import libraries here
+// My attempt at my initials
 
-// My attemp at my initials
+import java.util.Scanner;
 
 public class Assignment1 {
   public static void main(String[] args) {
+
+    final int BASE = 32;
+      final double CONVERSION_FACTOR = 9.0 / 5.0;
+
     System.out.println("DDDDDDDDDDDDDDDDDD            DDDDDDDDDDDDDDDDDDDDDDDDDDD");
     System.out.println("DDDDDDDDDDDDDDDDDDDDD       DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
     System.out.println("DDDDD            DDDDDD     DDDDDD       DDDDDDD     DDDDDD");
@@ -14,7 +18,36 @@ public class Assignment1 {
     System.out.println("DDD                DDDDD    DDDDD         DDDDD       DDDDD");
     System.out.println("DDDDD            DDDDDD     DDDDD         DDDDD       DDDDD");
     System.out.println("DDDDDDDDDDDDDDDDDDDDD       DDDDD         DDDDD       DDDDD");
-    System.out.println("DDDDDDDDDDDDDDDDDDD         DDDDD         DDDDD       DDDDD");
+    System.out.println("DDDDDDDDDDDDDDDDDDD         DDDDD         DDDDD       DDDDD\n");
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Please enter 5-character string
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    String message;
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Please enter a number in Fahrenheit:");
+
+    message = scan.nextLine();
+
+    System.out.println("You entered: \"" + message + "\"");
+    scan.close();
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Converter
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+      
+  
+      double fahrenheitTemp;
+      int celsiusTemp = 24;  // value to convert
+  
+      fahrenheitTemp = celsiusTemp * CONVERSION_FACTOR + BASE;
+  
+      System.out.println("Celsius Temperature: " + celsiusTemp);
+      System.out.println("Fahrenheit Equivalent: " + fahrenheitTemp);
+
 
   }
 }
