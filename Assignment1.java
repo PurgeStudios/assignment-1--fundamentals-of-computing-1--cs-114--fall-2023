d612ac704b5f9f36617988ee6fce6ea2ac7d620d
Dimitri <56205878+PurgeStudios@users.noreply.github.com>
2024-01-06 18:00:30 -0500

    Tested if Statement

--- Assignment1.java
+++ Assignment1.java
@@ -23,12 +23,12 @@ public class Assignment1 {
     System.out.println("DDD               DDDDD     MMMMMM     MMMMMMMMM     MMMMMM");
     System.out.println("DDD                DDDDD    MMMMM        MMMMMM       MMMMM");
     System.out.println("DDD                 DDDDD   MMMMM        MMMMMM       MMMMM");
-    System.out.println("DDD                DDDDD    MM0MMM        MMMMMM       MMMMM");
+    System.out.println("DDD                DDDDD    MMMMM        MMMMMM       MMMMM");
     System.out.println("DDD               DDDDD     MMMMM        MMMMMM       MMMMM");
     System.out.println("DDDD           DDDDDD       MMMMM        MMMMMM       MMMMM");
     System.out.println("DDDDDDDDDDDDDDDDDDDDD       MMMMM        MMMMMM       MMMMM");
     System.out.println("DDDDDDDDDDDDDDDDDDD         MMMMM        MMMMMM       MMMMM");
-    System.out.println('\n');
+     System.out.println();
 
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // Code for Users input
@@ -36,28 +36,38 @@ public class Assignment1 {
 
     Scanner scan = new Scanner(System.in);
 
-    System.out.println("Please enter a 5-character string:");
+    System.out.print("Please enter a 5-character string: ");
 
     message = scan.nextLine();
+
+    if (message.length() > 5 || message.length() > 5) {
+      System.out.println("Input either exceeds or is below 5 characters.");
+    }
+
+
+    System.out.println();
 
     messagefirst = message.substring(3,4);
     messagesecond = message.substring(2,3);
     messagethird = message.substring(1,2);
 
-    System.out.println("Please enter a number in Fahrenheit:");
+    System.out.print("Please enter a number in Fahrenheit: ");
 
     userInput = scan.nextInt();
+    System.out.println();
 
     fahrenheitTemp = userInput;
 
     celsiusTemp = (fahrenheitTemp -BASE)* CONVERSION_FACTOR;
 
-    System.out.println("Random number generated. Continuing...: ");
+    System.out.println("Random number generated. Continuing...");
+    System.out.println();
 
     randomNum = generator.nextInt(16384)+ 32;
 
     System.out.println("Your new string is: " + celsiusTemp + messagefirst + messagesecond + messagethird + randomNum);
+    System.out.println();
 
     scan.close();
+    }
   }
-}
